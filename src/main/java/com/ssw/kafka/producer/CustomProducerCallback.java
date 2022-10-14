4fe6f0071bad9f18914ec6e846a5e437454c537e
sswdr <771020712@qq.com>
2022-10-14 16:53:15 +0800

    study

--- src/main/java/com/ssw/kafka/producer/CustomProducerCallback.java
+++ src/main/java/com/ssw/kafka/producer/CustomProducerCallback.java
@@ -11,30 +11,28 @@ import java.util.Properties;
  */
 public class CustomProducerCallback {
     public static void main(String[] args) throws InterruptedException {
-        // 1. 创建 kafka 生产者的配置对象
+        // 1. 配置：创建 kafka 生产者的配置对象
         Properties properties = new Properties();
-        // 2. 给 kafka 配置对象添加配置信息：bootstrap.servers
+        // 2. 连接集群：给 kafka 配置对象添加配置信息：bootstrap.servers
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "ubuntu-20.04.wsl:9092,ubuntu-20.04.wsl:9094,ubuntu-20.04.wsl:9094");
-        // key,value 序列化 (必须)：key.serializer，value.serializer
+        // 3.指定key,value 序列化 (必须)：key.serializer，value.serializer
         // properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         // properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // 3. 创建 kafka 生产者对象
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
         // 4. 调用 send 方法,发送消息
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 5; i++) {
+            // topic: first,value: hello kafka
             kafkaProducer.send(new ProducerRecord<>("first", "hello kafka" + i), new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata metadata, Exception exception) {
-
-                    if (exception == null){
-                        System.out.println("主题： "+metadata.topic() + " 分区： "+ metadata.partition());
+                    if (exception == null) {
+                        System.out.println("主题： " + metadata.topic() + " 分区： " + metadata.partition());
                     }
                 }
             });
-
-            Thread.sleep(2);
         }
         // 5. 关闭资源
         kafkaProducer.close();
